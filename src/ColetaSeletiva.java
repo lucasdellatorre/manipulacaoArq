7e8244d71b6e1624112a3259d41268146d918fd2
lucasdellatorre <lucasdellatorre222@gmail.com>
2021-10-16 15:17:10 -0300

    Adicionando a classe ColetaSeletiva

---
+++ src/ColetaSeletiva.java
@@ -0,0 +1,76 @@
+public class ColetaSeletiva
+{
+    private String dataExtracao;
+    private String categoria;
+    private String codLogradouro;
+    private String diasColeta;
+    private int imparFim;
+    private int imparInicio;
+    private String lado;
+    private String nomeLogradouro;
+    private int parFim;
+    private int parInicio;
+    private String area;
+
+    public ColetaSeletiva
+            (String dataExtracao, String categoria, String codLogradouro,
+             String diasColeta, int imparFim, int imparInicio, String lado,
+             String nomeLogradouro, int parFim, int parInicio, String area)
+    {
+        this.dataExtracao = dataExtracao;
+        this.categoria = categoria;
+        this.codLogradouro = codLogradouro;
+        this.diasColeta = diasColeta;
+        this.imparFim = imparFim;
+        this.imparInicio = imparInicio;
+        this.lado = lado;
+        this.nomeLogradouro = nomeLogradouro;
+        this.parFim = parFim;
+        this.parInicio = parInicio;
+        this.area = area;
+    }
+
+    public String getDataExtracao() {
+        return dataExtracao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getCodLogradouro() {
+        return codLogradouro;
+    }
+
+    public String getDiasColeta() {
+        return diasColeta;
+    }
+
+    public int getImparFim() {
+        return imparFim;
+    }
+
+    public int getImparInicio() {
+        return imparInicio;
+    }
+
+    public String getLado() {
+        return lado;
+    }
+
+    public String getNomeLogradouro() {
+        return nomeLogradouro;
+    }
+
+    public int getParFim() {
+        return parFim;
+    }
+
+    public int getParInicio() {
+        return parInicio;
+    }
+
+    public String getArea() {
+        return area;
+    }
+}
